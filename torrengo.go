05716f783d0b4fa9b82c69cc818ab8d06164aa60
juju <julien.enilrahc@gmail.com>
2018-06-25 10:02:53 +0200

    Implement a basic CLI with arguments and flags.

--- torrengo.go
+++ torrengo.go
@@ -1,27 +1,62 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/juliensalinas/torrengo/arc"
 )
 
-// func checkInput(in string) error {
+func clean(in string) (string, error) {
 
-// 	if in == "" {
-// 		return fmt.Errorf("user input should not be empty")
-// 	}
+	// Clean user input by removing useless spaces.
+	clIn := strings.TrimSpace(in)
 
-// 	return nil
-// }
+	// If user input is empty raise an error.
+	if clIn == "" {
+		return "", fmt.Errorf("user input should not be empty")
+	}
+
+	return clIn, nil
+}
 
 func main() {
 
+	// Show line number during logging.
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	torrents, err := arc.Search("Dumas")
+	// Get command line flags and arguments.
+	websitePtr := flag.String("w", "all", "website you want to search: archive | all")
+	flag.Parse()
+	args := flag.Args()
+
+	// If no command line argument is supplied, then we stop here.
+	if len(args) == 0 {
+		os.Exit(1)
+	}
+
+	// Concatenate all arguments into one single string in case user does not use quotes.
+	in := strings.Join(args, " ")
+
+	// Clean user input.
+	clIn, err := clean(in)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	var torrents [][2]string
+
+	switch *websitePtr {
+	case "archive":
+		torrents, err = arc.Search(clIn)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "all":
+		fmt.Println("all")
 	}
 
 	log.Printf("%v", torrents)
