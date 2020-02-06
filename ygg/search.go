606fd669cb9c382b3a4ca233a0f512e7815f3732
Clément Bourgoin <c@iwzr.fr>
2020-02-06 10:59:23 +0100

    Update yggtorrent url

--- ygg/search.go
+++ ygg/search.go
@@ -45,7 +45,8 @@ import (
 // const baseURL = "yggtorrent.to"
 // const baseURL = "www2.yggtorrent.gg"
 // const baseURL = "www2.yggtorrent.ch"
-const baseURL = "www2.yggtorrent.ws"
+// const baseURL = "www2.yggtorrent.ws"
+const baseURL = "www2.yggtorrent.se"
 
 // searchURL is the url used to retrieve a list of torrents based on user keywords.
 // A typical final url looks like:
